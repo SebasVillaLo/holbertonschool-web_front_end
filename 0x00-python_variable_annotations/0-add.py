f502b459614e7656933d19c29b0ecf6186584c17
SebasVillaLo <villalopezsebastian@gmail.com>
2022-06-07 19:10:13 -0500

    ADD: All task's

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+add function with annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    returns the sum of two floats (a, b)
+    """
+    return a + b
